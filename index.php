92e491ccabf898fa9c3120c915a54c718e232134
hafi0406 <hafihanafi312@gmail.com>
2022-11-23 09:31:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio esse ducimus cum accusantium autem tempore nemo quas sequi officiis officia! Velit eveniet at magnam accusantium sint vitae omnis, odit provident.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
